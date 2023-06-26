5826db89fdad05259f4a4073294507cda39e512a
thekingvice <villarrealvicente92@gmail.com>
2023-06-25 19:06:30 -0700

    commit

---
+++ Sandbox/src/components/scroll-animation.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ScrollAnimation() {
+  return (
+    <div>
+      <section></section>
+      <section></section>
+      <section></section>
+    </div>
+  );
+}
