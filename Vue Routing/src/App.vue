de7f7e6f9305cffbc6f843fe019f8ef2a5b6a845
thekingvice <villarrealvicente92@gmail.com>
2023-07-18 20:31:21 -0700

    render multiple links and make multiple pages

---
+++ Vue Routing/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Hello World</div>
+  <a href="/">/Home</a>
+  <a href="/About">/About</a>
+  <router-view to="/"></router-view>
+</template>
+
+<style scoped></style>
---
+++ Vue Routing/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="About">About</div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
