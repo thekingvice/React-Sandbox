c0629b86b854dcfb5959e498066b6d0793b78715
thekingvice <villarrealvicente92@gmail.com>
2023-08-07 19:48:00 -0700

    commit

---
+++ JS Files/Test.js
@@ -0,0 +1,8 @@
+let number = 5;
+
+function NewFunction(number) {}
+
+NewFunction = () => {
+  console.log("TS Rules!!!");
+};
+console.log(NewFunction());
---
+++ TS Files/src/Test.ts
@@ -0,0 +1,8 @@
+let number: number = 56545453123456;
+function NewFunction(number: number) {
+  let newNumber: number = 0;
+  newNumber = number * 3;
+  return newNumber;
+}
+
+console.log(NewFunction(number));
