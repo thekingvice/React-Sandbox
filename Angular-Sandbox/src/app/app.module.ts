093d985aefe239711ae04248d2800b0ac6c73c7b
thekingvice <villarrealvicente92@gmail.com>
2023-08-15 21:31:52 -0700

    continued material testing

--- Angular-Sandbox/src/app/app.module.ts
+++ Angular-Sandbox/src/app/app.module.ts
@@ -6,19 +6,29 @@ import { CounterComponent } from './counter/counter.component';
 import { AccordianComponent } from './accordian/accordian.component';
 import { PropsComponent } from './props/props.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     AccordianComponent,
-    PropsComponent
+    PropsComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
